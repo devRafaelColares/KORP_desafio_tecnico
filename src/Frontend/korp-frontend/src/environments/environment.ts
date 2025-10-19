0eaf670ac11b4fbf618d8a5b7faf251c48548ffe
devRafaelColares <rafaelcolares.dev@gmail.com>
2025-10-19 12:24:23 -0300

    fix(frontend): corrigir baseUrl do EstoqueService no environment de produção
    Ajusta a variável estoqueServiceUrl para incluir o prefixo '/estoque-service/v1', garantindo que o frontend consuma corretamente os endpoints REST do backend de produtos.

--- src/Frontend/korp-frontend/src/environments/environment.ts
+++ src/Frontend/korp-frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    estoqueServiceUrl: 'http://localhost:5127',
-    faturamentoServiceUrl: 'http://localhost:5042',
+    estoqueServiceUrl: 'http://localhost:5127/estoque-service/v1',
+    faturamentoServiceUrl: 'http://localhost:5042/faturamento-service/v1',
     apiTimeout: 30000
 };
